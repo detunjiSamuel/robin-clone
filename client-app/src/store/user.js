15775e31025ab7e4888dfb88336d198689360177
Adetunji Samuel <samadetunji01@gmail.com>
2023-09-12 09:06:08 +0100

    add user reducer base

--- client-app/src/store/user.js
+++ client-app/src/store/user.js
@@ -1,8 +1,36 @@
-const SET_USER = "";
-const DELETE_USER = "";
+const SET_USER = "user/set";
+const DELETE_USER = "user/delete";
+
+const storeDispatchs = {
+  setUser: (user) => {
+    return {
+      type: SET_USER,
+      user,
+    };
+  },
+
+  deleteUser: (user) => {
+    return {
+      type: DELETE_USER,
+    };
+  },
+};
+
+/** ACTIONS */
+
 
 const userReducer = (state = {}, action) => {
   switch (action.type) {
+    case SET_USER: {
+      return {
+        user: action.user,
+      };
+    }
+    case DELETE_USER: {
+      return {
+        user: null,
+      };
+    }
     default:
       return state;
   }
