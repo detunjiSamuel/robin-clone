10030c7afca41f7706fbdd59b867c78360dec693
Adetunji Samuel <samadetunji01@gmail.com>
2023-09-20 02:16:08 +0100

    add stock model

--- models.py
+++ models.py
@@ -61,3 +61,35 @@ class Asset(db.Model):
             "quantity": self.quantity,
             "avgPrice": self.avg_price
         }
+
+
+
+class StockSymbol(db.Model):
+    __tablename__ = 'stock_symbols'
+        
+    id = db.Column(db.Integer, primary_key=True)
+    stock_symbol = db.Column(db.String(10), nullable=False)
+    company = db.Column(db.String, nullable=False)
+
+
+class WatchList(db.Model):
+    __tablename__ = 'watchlists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", back_populates="watchlists")
+    watchlist_stocks = db.relationship("WatchList_Stock",
+                             back_populates="watchlist",
+                             cascade="all, delete-orphan")
+
+    def json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id,
+            'watchlist_stocks': [stock.to_dict() for stock in self.watchlist_stocks]
+        }
