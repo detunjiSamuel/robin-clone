81f357960a31f22cf8170f39791e7a0d499b8e60
Adetunji Samuel <samadetunji01@gmail.com>
2023-09-20 03:27:39 +0100

    add transaction form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms.validators import DataRequired, Length, NumberRange , ValidationError
 
 
 from .validators import user_exists_login, password_matches_login, user_exists_signup
@@ -22,3 +22,24 @@ class SignUpForm(FlaskForm):
                            DataRequired(), Length(min=10)])
     networth = FloatField(
         "Networth", [DataRequired(), NumberRange(max=10000000)])
+
+class TransactionForm(FlaskForm):
+    symbol = StringField("Symbol", [DataRequired()])
+    transaction_type = StringField("Symbol", [DataRequired(message="Ammount cannot be 0")])
+    price = FloatField("Price", [DataRequired()])
+    quantity = FloatField("Quantity", [DataRequired()])
+
+def checkZero():
+    def _checkZero(form, field):
+        if type(field.data) is float and field.data >= 0.00:
+            return True
+        else:
+            raise ValidationError("Amount cannot be less than 0")
+    return _checkZero
+
+
+class AssetForm(FlaskForm):
+    symbol = StringField("Symbol", [DataRequired()])
+    name = StringField("Name", [DataRequired()])
+    quantity = FloatField("Quantity", [checkZero()])
+    avg_price = FloatField("Average Price", [DataRequired()])
