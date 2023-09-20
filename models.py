82b85cbed660f9bbeff9c2e4fbd9f8260b1c4ba2
Adetunji Samuel <samadetunji01@gmail.com>
2023-09-20 02:22:32 +0100

    add transaction model

--- models.py
+++ models.py
@@ -62,8 +62,6 @@ class Asset(db.Model):
             "avgPrice": self.avg_price
         }
 
-
-
 class StockSymbol(db.Model):
     __tablename__ = 'stock_symbols'
         
@@ -71,6 +69,25 @@ class StockSymbol(db.Model):
     stock_symbol = db.Column(db.String(10), nullable=False)
     company = db.Column(db.String, nullable=False)
 
+class WatchList_Stock(db.Model):
+    __tablename__ = "watchlist_stocks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    watchlist_id = db.Column(db.Integer, db.ForeignKey("watchlists.id"), nullable=False)
+    stock_symbol = db.Column(db.String(10), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    watchlist = db.relationship("WatchList", back_populates="watchlist_stocks")
+
+    def json(self):
+        return {
+            'id': self.id,
+            'watchlist_id': self.watchlist_id,
+            'stock_symbol': self.stock_symbol
+        }
+    
 
 class WatchList(db.Model):
     __tablename__ = 'watchlists'
@@ -93,3 +110,49 @@ class WatchList(db.Model):
             'user_id': self.user_id,
             'watchlist_stocks': [stock.to_dict() for stock in self.watchlist_stocks]
         }
+
+class News(db.Model):
+    __tablename__ = 'news'
+
+    id = db.Column(db.Integer, primary_key=True)
+    like = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey(
+                            'users.id'
+                        ), nullable=False)
+    title = db.Column(db.String, nullable=False)
+    source = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    ticker = db.Column(db.String)
+    article_link = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", back_populates='news')
+
+    def json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "source": self.source,
+            "image": self.image,
+            "ticker": self.ticker,
+            "article_link": self.article_link,
+        }
+    
+class Transaction(db.Model):
+    __tablename__ = 'transactions'
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    symbol = db.Column(db.String(10), nullable=False)
+    # sell or buy
+    transaction_type = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+    transaction_time = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship("User", back_populates='transactions')
